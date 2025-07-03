3da725b2fc67e01a30c91d791815a38b7219fee9
Diano <diano.faniry@gmail.com>
2025-07-03 22:04:33 +0400

    sidebar et navbar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "./style.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// src/index.js
+// Import de Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import passionImg from './assets/img/passion.jpg';
+import websiteImg from './assets/img/website.jpg';
+import * as bootstrap from 'bootstrap';
 
