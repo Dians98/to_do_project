97d4feb87da21ba98e4c83ac88f9093ab27c722b
Diano <diano.faniry@gmail.com>
2025-07-29 22:01:16 +0400

    render dynamic view done

--- src/assets/js/dom.js
+++ src/assets/js/dom.js
@@ -459,7 +459,7 @@ function renderWeekTask(projects) {
     const lastSunday = new Date();
     //tout simplement, je recule de combien de jour pour avoir dimanche
     lastSunday.setDate(lastSunday.getDate() - lastSunday.getDay()); // Trouver le dimanche de la semaine en cours
-    
+
 
     //pour pouvoir comparer les dates, le meilleur moyen c'est d'utiliser un timestamp
     const lastSundayTimestamp = lastSunday.getTime();
@@ -839,14 +839,13 @@ export function add_event_listener_on_project_list(projects) {
             remove_all_active_menu();
             target.classList.add("active");
             const id = target.id;
-
+            renderDynamicProjectView(id, projects);
 
         }
     });
 }
 
 function renderDynamicView(id, projects) {
-
     switch (id) {
         case "all":
             renderAllTasks(projects);
@@ -863,6 +862,103 @@ function renderDynamicView(id, projects) {
         case "week":
             renderWeekTask(projects);
             break;
+    }
+}
+
+function renderDynamicProjectView(id, projects) {
+    const tasks_list_container = document.querySelector(".tasks_list")
+    const project = projects.find(project => project.id == id);
+
+    tasks_list_container.innerHTML = ''
+
+    let taskNumber = 0
+    if (project) {
+        const tasks = project.tasks
+        document.querySelector("#dynamic_to_do_title").textContent = project.name;
+        tasks.forEach((task, index) => {
+
+            const taskItem = document.createElement("div")
+            taskItem.className = "task_item"
+
+            if (task.done) {
+                taskItem.classList.add("done")
+            } else {
+                taskItem.classList.remove("done")
+            }
+
+            const taskInfo = document.createElement("div")
+            taskInfo.className = "task_info"
+
+            const doneBtn = document.createElement("button")
+            doneBtn.className = "btn-done"
+            doneBtn.style.border = "none"
+            doneBtn.style.background = "transparent"
+            doneBtn.style.cursor = "pointer"
+            doneBtn.dataset.taskId = task.id
+
+
+            // fonction pour mettre à jour l'icône selon l'état done
+            function updateIcon() {
+                doneBtn.innerHTML = task.done
+                    ? `<i class="fas fa-check-circle" style="color:#f6d365"></i>`
+                    : `<i class="far fa-circle" style="color:#fda085"></i>`
+            }
+
+            updateIcon()
+
+            addEventListenerOnDoneBtn(doneBtn, task, projects)
+
+            const taskTitle = document.createElement("div")
+            taskTitle.className = "task_title"
+            taskTitle.textContent = task.title
+
+            const task_actions = document.createElement("div")
+            task_actions.className = "task_actions"
+
+            // date
+            const dateDiv = document.createElement("div")
+            dateDiv.className = "task_date"
+            dateDiv.textContent = task.dueDate
+
+            const viewTask = document.createElement("div")
+            viewTask.className = "view_task"
+            viewTask.dataset.taskId = task.id
+            viewTask.innerHTML = `<i class="fas fa-info-circle" style="color : lightblue;"></i>`
+
+            viewTaskAddEventListener(viewTask, task)
+
+            const editTask = document.createElement("div")
+            editTask.className = "edit_task"
+            editTask.dataset.taskId = task.id
+            editTask.innerHTML = `<i class="far fa-edit" style="color : #f6d365;"></i>`
+
+            editTaskAddEventListener(editTask, task, datePicker1)
+
+            const deleteTask = document.createElement("div")
+            deleteTask.className = "delete_task"
+            deleteTask.dataset.taskId = task.id
+            deleteTask.innerHTML = `<i class="fas fa-trash-alt" style="color : #fda085;"></i>`
+
+            deleteTaskAddEventListener(deleteTask, task.id, projects)
+
+
+            task_actions.appendChild(dateDiv)
+            task_actions.appendChild(viewTask)
+            task_actions.appendChild(editTask)
+            task_actions.appendChild(deleteTask)
+
+            taskInfo.appendChild(doneBtn)
+            taskInfo.appendChild(taskTitle)
+
+            taskItem.appendChild(taskInfo)
+            taskItem.appendChild(task_actions)
+
+            tasks_list_container.appendChild(taskItem)
+
+            taskNumber++
+
+
+        })
     }
 }
 
